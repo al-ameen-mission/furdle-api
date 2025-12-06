74d6b8dbaedacbd72f480606df609ad986660524
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-12-06 18:44:36 +0530

    Remove unused Logger import in AuthController

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Helpers\TokenHelper;
-use App\Helpers\Logger;
 use App\Core\Request;
 use App\Core\Response;
 use App\Helpers\DbHelper;
 use App\Helpers\FaceApiHelper;
-use stdClass;
 
 /**
  * Auth Controller for handling authentication routes.
